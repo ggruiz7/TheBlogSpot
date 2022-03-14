8efe0f82206df683c66050db31290d21f24b4043
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-03-14 13:06:43 -0700

    changed bloglist to use db using fetch, removed defunct delete

--- src/componets/BlogList.js
+++ src/componets/BlogList.js
@@ -1,5 +1,5 @@
 // passing props through as arguments
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -7,7 +7,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
--- src/componets/Home.js
+++ src/componets/Home.js
@@ -2,27 +2,20 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "new blog", body: "lorem ipsum...", author: "gg", id: 1 },
-    { title: "welcome!", body: "lorem ipsum...", author: "nat", id: 2 },
-    { title: "info", body: "lorem ipsum...", author: "brud", id: 3 },
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("use effect ran");
-    console.log(blogs);
-  });
+    fetch("http://localhost:8000/blogs")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
 
-  return (
-    <div className="Home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-    </div>
-  );
+  return <div className="Home">{blogs && <BlogList blogs={blogs} title="All Blogs" />}</div>;
 };
 
 export default Home;
