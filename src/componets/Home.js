b8ecf07027f3851c9551d0b36b4a9bad439c72e6
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-03-14 13:48:52 -0700

    error handling added

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -4,21 +4,34 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((response) => {
+        console.log(response);
+
+        if (!response.ok) {
+          throw Error("could not fetch the data :/");
+        }
+
         return response.json();
       })
       .then((data) => {
         console.log(data);
         setIsPending(false);
         setBlogs(data);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <div className="Home">
+      {error && <div> {error} </div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
